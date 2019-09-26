ec74c14dd606dc8b76eed2759141911f0b9d3475
damien <d.macors@gmail.com>
2019-09-26 14:20:32 +0200

    boucle

--- script.js
+++ script.js
@@ -16,26 +16,69 @@
 
     // ctx.strokeRect(25, 25, 175, 200);
 
+    // function degToRad(degrees) {
+    //     return degrees * Math.PI / 180;
+    // };
+
+    // ctx.fillStyle = 'rgb(255, 0, 0)';
+    // ctx.beginPath();
+    // ctx.moveTo(50, 50);
+
+    // ctx.lineTo(150, 50);
+    // let triHeight = 50 * Math.tan(degToRad(60));
+    // ctx.lineTo(100, 50 + triHeight);
+    // ctx.lineTo(50, 50);
+    // ctx.fill();
+    // ctx.fillStyle = 'rgb(0, 0, 255)';
+    // ctx.beginPath();
+    // ctx.arc(150, 106, 50, degToRad(0), degToRad(360), false);
+    // ctx.fill();
+    // ctx.fillStyle = 'yellow';
+    // ctx.beginPath();
+    // ctx.arc(200, 106, 50, degToRad(-45), degToRad(45), true);
+    // ctx.lineTo(200, 106);
+    // ctx.fill();
+
+    // ctx.strokeStyle = 'white';
+    // ctx.lineWidth = 1;
+    // ctx.font = '36px arial';
+    // ctx.strokeText('Canvas text', 50, 50);
+
+    // ctx.fillStyle = 'red';
+    // ctx.font = '48px georgia';
+    // ctx.fillText('Canvas text', 50, 150);
+
+    // let image = new Image();
+    // image.src = 'firefox.png';
+    // image.onload = function() {
+    //     ctx.drawImage(image, 20, 20, 185, 175, 50, 50, 185, 175);
+
+    // }
+    ctx.translate(width / 2, height / 2);
+
     function degToRad(degrees) {
         return degrees * Math.PI / 180;
     };
 
-    ctx.fillStyle = 'rgb(255, 0, 0)';
-    ctx.beginPath();
-    ctx.moveTo(50, 50);
+    function rand(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + (min);
+    }
 
-    ctx.lineTo(150, 50);
-    let triHeight = 50 * Math.tan(degToRad(60));
-    ctx.lineTo(100, 50 + triHeight);
-    ctx.lineTo(50, 50);
-    ctx.fill();
-    ctx.fillStyle = 'rgb(0, 0, 255)';
-    ctx.beginPath();
-    ctx.arc(150, 106, 50, degToRad(0), degToRad(360), false);
-    ctx.fill();
-    ctx.fillStyle = 'yellow';
-    ctx.beginPath();
-    ctx.arc(200, 106, 50, degToRad(-45), degToRad(45), true);
-    ctx.lineTo(200, 106);
-    ctx.fill();
+    let length = 250;
+    let moveOffset = 20;
+
+    for (var i = 0; i < length; i++) {
+        ctx.fillStyle = 'rgba(' + (255 - length) + ', 0, ' + (255 - length) + ', 0.9)';
+        ctx.beginPath();
+        ctx.moveTo(moveOffset, moveOffset);
+        ctx.lineTo(moveOffset + length, moveOffset);
+        let triHeight = length / 2 * Math.tan(degToRad(60));
+        ctx.lineTo(moveOffset + (length / 2), moveOffset + triHeight);
+        ctx.lineTo(moveOffset, moveOffset);
+        ctx.fill();
+
+        length--;
+        moveOffset += 0.7;
+        ctx.rotate(degToRad(5));
+    }
 })();
